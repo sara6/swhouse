6c04e46b37229cb020e1ff597761109edb68a0b7
Apple <sarah.crawley6@gmail.com>
2016-04-27 22:13:27 +0100

    Gulp file actually sorted out

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,8 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
-
-// var sassPaths = [
-//   'bower_components/foundation-sites/scss',
-//   'bower_components/motion-ui/src'
-// ];
+var open = require('gulp-open');
 
 gulp.task('open', function(){
   gulp.src('./index.html')
   .pipe(open());
 });
-
-// gulp.task('sass', function() {
-//   return gulp.src('scss/app.scss')
-//     .pipe($.sass({
-//       includePaths: sassPaths
-//     })
-//       .on('error', $.sass.logError))
-//     .pipe($.autoprefixer({
-//       browsers: ['last 2 versions', 'ie >= 9']
-//     }))
-//     .pipe(gulp.dest('css'));
-// });
-//
-// gulp.task('default', ['sass'], function() {
-//   gulp.watch(['scss/**/*.scss'], ['sass']);
-// });
