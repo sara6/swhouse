9d3a8eb0721ff45385cea708c3ac5dfcdf5a49a8
Apple <sarah.crawley6@gmail.com>
2016-04-27 22:00:12 +0100

    Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-// var gulp = require('gulp');
-// var $    = require('gulp-load-plugins')();
-//
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
 // var sassPaths = [
 //   'bower_components/foundation-sites/scss',
 //   'bower_components/motion-ui/src'
 // ];
-//
+
+gulp.task('open', function(){
+  gulp.src('./index.html')
+  .pipe(open());
+});
+
 // gulp.task('sass', function() {
 //   return gulp.src('scss/app.scss')
 //     .pipe($.sass({
