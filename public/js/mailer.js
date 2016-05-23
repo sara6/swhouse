a7d9bab1979f53b9256df62b314314c3683b4279
Em01 <emma_williams1@hotmail.co.uk>
2016-05-23 21:28:12 +0100

    Update with xoauth2

--- public/js/mailer.js
+++ public/js/mailer.js
@@ -1,32 +1,30 @@
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
-var smtpTransport = nodemailer.createTransport("SMTP",{
-   service: "Gmail",  // sets automatically host, port and connection security settings
-   auth: {
-       user: "",
-       pass: ""
-   }
+var smtpTransport = nodemailer.createTransport("SMTP", {
+  service: "Gmail",
+  auth: {
+    XOAuth2: {
+      user: "your_email_address@gmail.com",             
+      clientId: "your_client_id",
+      clientSecret: "your_client_secret",
+      refreshToken: "your_refresh_token"
+    }
+  }
 });
 
-smtpTransport.sendMail({  //email options
-   from: "Sender Name <email@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
-   to: "Receiver Name <receiver@email.com>", // receiver
-   subject: "Emailing with nodemailer", // subject
-   text: "Email Example with nodemailer" // body
-}, function(error, response){  //callback
-   if(error){
-       console.log(error);
-   }else{
-       console.log("Message sent: " + response.message);
-   }
-   
-   smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
-});
+var mailOptions = {
+  from: "your_email_address@gmail.com",
+  to: "my_another_email_address@gmail.com",
+  subject: "Hello",
+  generateTextFromHTML: true,
+  html: "<b>Hello world</b>"
+};
 
-var smtpTransport = nodemailer.createTransport("SMTP",{
-   service: "Gmail",  // sets automatically host, port and connection security settings
-   auth: {
-       user: "email@gmail.com",
-       pass: "gmailPassword"
-   }
+smtpTransport.sendMail(mailOptions, function(error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response);
+  }
+  smtpTransport.close();
 });
